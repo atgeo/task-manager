489cc932efd259f63c7044b737377549ae00d33b
Georges Kmeid <georgesk117@gmail.com>
2024-10-10 11:01:27 -0400

    Add endpoint to retrieve tasks for the authenticated user

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskCreateView
+from .views import TaskCreateView, UserTaskListView
 
 urlpatterns = [
     path('add/', TaskCreateView.as_view(), name='task-add'),
+    path('my-tasks/', UserTaskListView.as_view(), name='task-list'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,16 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
 
 
 class TaskCreateView(generics.CreateAPIView):
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class UserTaskListView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
