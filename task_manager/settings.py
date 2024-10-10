8b0109ea7735338ffa75b9f47f9f57d7d4891069
Georges Kmeid <georgesk117@gmail.com>
2024-10-10 11:54:05 -0400

    Install django-environ and add .env.dist for environment variable management

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,19 +11,27 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s@7-hfp8o68$l^xuy9kje@v-mbf%nm1&ybsqhqe4a&(fc-gnte'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
