6b6fba87c9b3912d7551df15d704d199b3b475f3
Georges Kmeid <georgesk117@gmail.com>
2024-10-12 15:05:38 -0400

    Add priority to Task

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,6 +15,7 @@ class Task(models.Model):
     due_date = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, null=True, blank=True)
 
     def __str__(self):
         return self.title
